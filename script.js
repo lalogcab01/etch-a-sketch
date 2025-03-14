e72f66904d95760cacf94db031dc4c0d46a1c832
eduardo <152088936+lalogcab01@users.noreply.github.com>
2025-03-13 19:34:18 -0600

    Created square grid 16x16

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const grid = document.querySelector(".grid");
 
 for (i = 0; i < 16; i++) {
-  let square = document.createElement("div");
-  square.setAttribute("class", "square");
-  grid.appendChild(square);
+  let row = document.createElement("div");
+  row.setAttribute("class", "row");
+  grid.appendChild(row);
+  for (j = 0; j < 16; j++) {
+    let square = document.createElement("div");
+    square.setAttribute("class", "square");
+    row.appendChild(square);
+  }
 }
