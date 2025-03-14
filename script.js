67d0e1ecf6a23971784c58a91a6ef97950bfca23
eduardo <152088936+lalogcab01@users.noreply.github.com>
2025-03-13 20:10:08 -0600

    Introduced process of creating new grid

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const grid = document.querySelector(".grid");
+function createGrid() {
+  size = getGridSize();
+  console.log(size);
+}
+function getGridSize() {
+  grid.innerHTML = "";
 
-for (i = 0; i < 16; i++) {
-  let row = document.createElement("div");
-  row.setAttribute("class", "row");
-  grid.appendChild(row);
-  for (j = 0; j < 16; j++) {
-    let square = document.createElement("div");
-    square.setAttribute("class", "square");
-    row.appendChild(square);
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "blue";
-    })
-    // square.addEventListener("mouseout", () => {
-    //   square.style.backgroundColor = "transparent";
-    // })
+  size = prompt("Introduce the size of the grid (number of squares per side):");
+  
+  for (i = 1; i <= size; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    grid.appendChild(row);
+    for (j = 1; j <= size; j++) {
+      let square = document.createElement("div");
+      square.setAttribute("class", "square");
+      row.appendChild(square);
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "blue";
+      })
+      // square.addEventListener("mouseout", () => {
+      //   square.style.backgroundColor = "transparent";
+      // })
+    }
   }
 }
+
+const button = document.querySelector("button");
+const grid = document.querySelector(".grid");
+
+button.addEventListener("click", createGrid);
 
